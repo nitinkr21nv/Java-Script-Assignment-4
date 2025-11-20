3b037d8b1b9410c1dab1c8dc5974f13bae3c1f4a
Nitin Kumar <nitin2111kr@gmail.com>
2025-11-20 15:30:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+let a = prompt("Please enter the first number:");
+let b = prompt("Please enter the second number:");
+let sum = Number(a) + Number(b);
+console.log(sum);
+alert(`The addition of the given number ${a} and ${b} is = ${sum}`);
+
